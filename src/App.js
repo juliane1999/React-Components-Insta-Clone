b23218db6d1ba2291c91fe39b4ed19dea04f05c3
juliane1999 <lastjuliane@gmail.com>
2021-07-13 16:41:57 -0700

    completed the posts.js section

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ const App = () => {
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       <SearchBar />
-      <Posts likePost = {posts}/>
+      <Posts likePost = {posts} posts={posts}/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
